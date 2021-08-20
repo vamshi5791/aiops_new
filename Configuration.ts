c07f73a5dae34f153c8c36d66df4e0f39d2dfc74
unknown <177777@ust-global.com>
2021-08-20 21:09:18 +0530

    updated feature files

--- Configuration.ts
+++ Configuration.ts
@@ -47,10 +47,10 @@ export let config: Config = {
     '../Features/LogIn/LogIn_IE.feature',
     '../Features/QueueChannel/CreateQueqeChannel_IE.feature',
     '../Features/LogInPersona/LoginPersona_IE.feature',
-    // '../Features/ProjectInstallation/ProjectInstallation_IE.feature',
+    // // '../Features/ProjectInstallation/ProjectInstallation_IE.feature',
     '../Features/Logout/LogOut.feature',
 
-    // Engineer
+    // // Engineer
     '../Features/LogIn/LogIn_ITOps_Engineer.feature',
     '../Features/LogInPersona/LoginPersona_ITOpsEngineer.feature',
     '../Features/DeviceInventory/Device_Inventory_Engineer.feature',
@@ -63,12 +63,12 @@ export let config: Config = {
     '../Features/TicketingThreshold/TicketingThreshold_ITOpsEngineer.feature',
     '../Features/Logout/LogOut.feature',
 
-    // Visitor
+    // // Visitor
     '../Features/LogIn/LogIn_ITOps_Visitor.feature',
     '../Features/LogInPersona/LoginPersona_ITOpsVisitor.feature',
     '../Features/Logout/LogOut.feature',
 
-    // // Admin
+    // // // Admin
     '../Features/LogIn/LogIn_Admin.feature',
     '../Features/LogInPersona/LoginPersona_ITOpsAdmin.feature',
     '../Features/DeviceInventory/Device_Inventory_Admin.feature',
@@ -81,14 +81,15 @@ export let config: Config = {
     '../Features/DisplayConfiguration/DisplayConfiguration.feature',
     '../Features/TicketingThreshold/TicketingThreshold_ITOpsAdmin.feature',
     '../Features/QueueChannel/CreateQueqeChannel_admin.feature',
-    // '../Features/Policies/AlertCorrelation.feature',
-    // '../Features/Policies/Acknowledgement.feature',
-    // '../Features/Policies/Failure.feature',
-    // '../Features/Policies/Recovery.feature',
-    // '../Features/ProjectInstallation/ProjectInstallation_Admin.feature',
     '../Features/Pushing_Alerts/PushingAlerts.feature',
     '../Features/Logout/LogOut.feature',
-
+ // Admin
+    '../Features/LogIn/LogIn_Admin.feature',
+   '../Features/Policies/AlertCorrelation.feature',
+    '../Features/Policies/Acknowledgement.feature',
+    '../Features/Policies/Failure.feature',
+    '../Features/Policies/Recovery.feature',
+    // '../Features/ProjectInstallation/ProjectInstallation_Admin.feature',
   ],
   cucumberOpts: {
 
