a39e4b2478e3b0d3b0870caf100caaa5fa0e7655
unknown <177778@ust-global.com>
2021-08-27 12:14:44 +0530

    Updated severity mapping file

--- Step_Definitions/SeverityMapping/Severity_Mapping.ts
+++ Step_Definitions/SeverityMapping/Severity_Mapping.ts
@@ -159,59 +159,104 @@ Then('Success message for Severity Mapping must be shown in popup {string}', asy
 //  Verify_Severity_Mapping_Page_For_ITOps_Admin
 
 Then('{string} verifies Source Severity text feild as {string}', async function (userRole, SourceSeverity) {
-
-  await element(by.xpath('//th[text()="SOURCE SEVERITY"]')).getText().then(function (text) {
-    expect(text).to.include(SourceSeverity);
-  });
+  try {
+    await element(by.xpath('//th[text()="SOURCE SEVERITY"]')).getText().then(function (text) {
+      expect(text).to.include(SourceSeverity);
+    });
+  }
+  catch (error) {
+    throw "User is not able verify Source Severity"
+  }
 });
 
 Then('{string} verifies SO Severity as {string}', async function (userRole, SOSeverity) {
-
-  await element(by.xpath('//th[text()="SO SEVERITY"]')).getText().then(function (text) {
-    expect(text).to.include(SOSeverity);
-  });
+  try {
+    await element(by.xpath('//th[text()="SO SEVERITY"]')).getText().then(function (text) {
+      expect(text).to.include(SOSeverity);
+    });
+  }
+  catch (error) {
+    throw "User is not able to  verify SO Severity"
+  }
 });
 
 Then('{string} verifies Updated time as {string}', async function (userRole, time) {
+  try {
+    await browser.wait(EC.visibilityOf(element(by.xpath('//th[text()="UPDATED TIME"]'))), 10000);
+  }
+  catch (error) {
+    throw "User is not able to  verifies Updated time"
+  }
 
-  await browser.wait(EC.visibilityOf(element(by.xpath('//th[text()="UPDATED TIME"]'))), 10000);
 
 });
 
 Then('{string} verifies +Add Severity as {string}', async function (userRole, AddSeverity) {
+  try {
+    await element(by.xpath('//span[text()=" Add Severity"]')).getText().then(function (text) {
+      expect(text).to.include(AddSeverity);
+    });
+  }
+  catch (error) {
+    throw "User is not able to verifies +Add Severity"
+  }
 
-  await element(by.xpath('//span[text()=" Add Severity"]')).getText().then(function (text) {
-    expect(text).to.include(AddSeverity);
-  });
 });
 
 Then('{string} verifies Delete icon', async function (userRole) {
+  try {
+    await element(by.xpath('//span[@class="smo smo-delete cursor-pointer font-18 color-grey left-margin-20 ng-star-inserted"]')).isDisplayed();
+  }
+  catch (error) {
+    throw "User is not able to verifies Delete icon"
+  }
 
-  await element(by.xpath('//span[@class="smo smo-delete cursor-pointer font-18 color-grey left-margin-20 ng-star-inserted"]')).isDisplayed();
 });
 
 Then('{string} verifies Add New Source as {string}', async function (userRole, popup6) {
+  try {
+    await element(by.xpath('//span[text()=" Add New Source"]')).getText().then(function (text) {
+      expect(text).to.include(popup6);
+    });
+  }
+  catch (error) {
+    throw "User is not able to verifies Add New Source"
+  }
 
-  await element(by.xpath('//span[text()=" Add New Source"]')).getText().then(function (text) {
-    expect(text).to.include(popup6);
-  });
 });
 
 Then('{string} verifies Edit icon', async function (userRole) {
+  try {
+    await element(by.xpath('//span[@class="smo smo-create-alt edit-icon cursor-pointer right-padding-20 ng-star-inserted"]')).isDisplayed();
 
-  await element(by.xpath('//span[@class="smo smo-create-alt edit-icon cursor-pointer right-padding-20 ng-star-inserted"]')).isDisplayed();
+  }
+  catch (error) {
+    throw "User is not able to verifies Edit icon"
+  }
+
 });
 
 Then('{string} verifies Delete icon of specefic Severity', async function (userRole) {
+  try {
+    await element(by.xpath('//span[@class="smo smo-delete trash-icon cursor-pointer ng-star-inserted"]')).isDisplayed();
 
-  await element(by.xpath('//span[@class="smo smo-delete trash-icon cursor-pointer ng-star-inserted"]')).isDisplayed();
+  }
+  catch (error) {
+    throw "User is not able to verifies Delete icon of specefic Severity"
+  }
+
 });
 
 Then('{string} verifies the Time as {string}', async function (userRole, Time) {
+  try {
+    await element(by.xpath('//b[text()="' + Time + '"]')).getText().then(function (text) {
+      expect(text).to.include(Time);
+    });
+  }
+  catch (error) {
+    throw "User is not able to  verifies the Time"
+  }
 
-  await element(by.xpath('//b[text()="' + Time + '"]')).getText().then(function (text) {
-    expect(text).to.include(Time);
-  });
 });
 
 
@@ -238,22 +283,51 @@ Then('Error message for creating duplicate source must be shown in popup {string
 // Verify_Duplicate_SourceSeverityandSOSeverity
 
 Given('{string} clicks on edit button', async function (userRole) {
-  await objSeverityMapping.Edit();
+  try {
+    await objSeverityMapping.Edit();
+  }
+  catch (error) {
+    throw "User is not able to clicks on edit button"
+  }
+
 });
 Given('{string} clicks on cancel button', async function (userRole) {
-  await objSeverityMapping.Cancel();
+  try {
+    await objSeverityMapping.Cancel();
+  }
+  catch (error) {
+    throw "User is not able to clicks on cancel button"
+  }
+
 });
 Given('{string} changes source Severity as {string}', async function (userRole, SourceSeverity) {
-  await objSeverityMapping.SourceSeverity(SourceSeverity);
+  try {
+    await objSeverityMapping.SourceSeverity(SourceSeverity);
+  }
+  catch (error) {
+    throw "User is not able to changes source Severity"
+  }
+
 });
 
 Given('{string} click on save button', async function (userRole) {
-  await objSeverityMapping.ClickOnSave();
+  try {
+    await objSeverityMapping.ClickOnSave();
+  }
+  catch (error) {
+    throw "User is not able to click on save button"
+  }
+
 });
 
 Given('{string} clicks on yes button', async function (userRole) {
-  await browser.sleep(2000)
-  await objSeverityMapping.ClickONYes();
+  try {
+    await browser.sleep(2000)
+    await objSeverityMapping.ClickONYes();
+  }
+  catch (error) {
+    throw "User is not able to clicks on yes button"
+  }
 
 });
 
@@ -268,7 +342,13 @@ Then('Error message for creating duplicate source severity must be shown in popu
 })
 
 Given('{string} changes another source Severity as {string}', async function (userRole, SourceSeverity) {
-  await objSeverityMapping.SourceSeverity(SourceSeverity);
+  try {
+    await objSeverityMapping.SourceSeverity(SourceSeverity);
+  }
+  catch (error) {
+    throw "User is not able to render the URL"
+  }
+
 })
 
 Then('Success message for updating Severity Mapping must be shown in popup {string}', async function (popup) {
@@ -294,22 +374,46 @@ Then('Success message for updating Severity Mapping must be shown in popup {stri
 
 
 Given('{string} clicks on + Add Severity button', async function (userRole) {
-  await objSeverityMapping.AddSeverity();
+  try {
+    await objSeverityMapping.AddSeverity();
+  }
+  catch (error) {
+    throw "User is not able to clicks on + Add Severity button"
+  }
+
 });
 
 
 Given('{string} clicks on save and add new button', async function (userRole) {
-  await objSeverityMapping.DropDownForSaveAndNew();
-  await objSeverityMapping.ClickOnSaveAndAddNew();
+  try {
+    await objSeverityMapping.DropDownForSaveAndNew();
+    await objSeverityMapping.ClickOnSaveAndAddNew();
+  }
+  catch (error) {
+    throw "User is not able to clicks on save and add new button"
+  }
+
 });
 
 Given('{string} clicks on SO Severity dropdown and selects Critical', async function (userRole) {
-  await objSeverityMapping.ChooseSOSeverity();
-  await objSeverityMapping.SelectCritical();
+  try {
+    await objSeverityMapping.ChooseSOSeverity();
+    await objSeverityMapping.SelectCritical();
+  }
+  catch (error) {
+    throw "User is not able to clicks on SO Severity dropdown and selects Critical"
+  }
+
 });
 
 Given('{string} clicks on save button', async function (userRole) {
-  await objSeverityMapping.Save();
+  try {
+    await objSeverityMapping.Save();
+  }
+  catch (error) {
+    throw "User is not able to clicks on save button"
+  }
+
 });
 
 Then('Success message for Verifyimg adding additional source severity within the same source must be shown in popup {string}', async function (popup) {
@@ -342,25 +446,49 @@ Then('Success message for Verifyimg adding additional source severity within the
 // Verifying ITOps Enginner is able to Configure New Severity Mapping
 
 Then('{string} verifies +Add Severity icon is not available {string}', async function (string, string2) {
-  await element(by.xpath('//b[text()="SOLARWINDS"]//following::span[@class="cursor-pointer ng-star-inserted"]')).isPresent().then(function (select) {
-    expect(select).to.be.false;
-  });
+  try {
+    await element(by.xpath('//b[text()="SOLARWINDS"]//following::span[@class="cursor-pointer ng-star-inserted"]')).isPresent().then(function (select) {
+      expect(select).to.be.false;
+    });
+  }
+  catch (error) {
+    throw " +Add Severity icon is available"
+  }
+
 });
 
 Then('{string} verifies Delete icon is not available', async function (string) {
-  await element(by.xpath('//span[@class="smo smo-delete cursor-pointer font-18 color-grey left-margin-20 ng-star-inserted"]')).isPresent().then(function (select) {
-    expect(select).to.be.false;
-  });
+  try {
+    await element(by.xpath('//span[@class="smo smo-delete cursor-pointer font-18 color-grey left-margin-20 ng-star-inserted"]')).isPresent().then(function (select) {
+      expect(select).to.be.false;
+    });
+  }
+  catch (error) {
+    throw "Delete icon is available"
+  }
+
 });
 
 Then('{string} verifies Add New Source icon is not available {string}', async function (string, string2) {
-  await element(by.xpath('//span[text()=" Add New Source"]')).isPresent().then(function (select) {
-    expect(select).to.be.false;
-  });
+  try {
+    await element(by.xpath('//span[text()=" Add New Source"]')).isPresent().then(function (select) {
+      expect(select).to.be.false;
+    });
+  }
+  catch (error) {
+    throw "Add New Source icon is available"
+  }
+
 });
 
 Then('{string} verifies Edit icon is not available', async function (string) {
-  await element(by.xpath('//span[@class="smo smo-create-alt edit-icon cursor-pointer right-padding-20 ng-star-inserted"]')).isPresent().then(function (select) {
-    expect(select).to.be.false;
-  });
+  try {
+    await element(by.xpath('//span[@class="smo smo-create-alt edit-icon cursor-pointer right-padding-20 ng-star-inserted"]')).isPresent().then(function (select) {
+      expect(select).to.be.false;
+    });
+  }
+  catch (error) {
+    throw "Edit icon is available"
+  }
+
 });
